54b7d9d13ed6d8a85e774ba5ef095c1ab4cdc48c
hyp <2905325171@qq.com>
2021-03-27 12:34:42 +0800

    初版提交补充

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap3/dist/css/bootstrap.min.css'
+import 'bootstrap3/dist/js/bootstrap.min'
+import axios from 'axios'
+import router from './router.js'
+
+import '@/assets/icons/iconfont.css'
+
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
